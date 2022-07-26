8d602810b83a53a614bb513b8455b69c846d2733
Mati Nemera <mnemera17@winona.edu>
2022-07-26 15:28:54 -0500

    django admin page setup

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import ToDoList,Item
 # Register your models here.
+#username: matinemera
+#password: 12345
+admin.site.register(ToDoList)
+admin.site.register(Item)
