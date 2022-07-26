b850ef5ce70971384116ba72258fb806b83e6e5f
Mati Nemera <mnemera17@winona.edu>
2022-07-26 17:48:55 -0500

    html edits

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import ToDoList,Item
 # Register your models here.
+
+
 #username: matinemera
 #password: 12345
 admin.site.register(ToDoList)
 admin.site.register(Item)
+# admin.site.register(Item.id)
